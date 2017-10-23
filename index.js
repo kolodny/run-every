d335b063953353554494e3b02c3357453660ab82
Max Ogden <max@maxogden.com>
2017-10-23 18:02:25 +0200

    subtract execution time from next execution

--- index.js
+++ index.js
@@ -12,11 +12,15 @@ function runEvery(repeatWaitSeconds, command, process) {
   inner();
 
   function inner() {
+    var start = Date.now();
     proc = spawn(command[0], command.slice(1), { stdio: 'inherit' });
     proc.on('exit', function (code, signal) {
-
+      var finished = Date.now();
       if (code === 0 && signal === null) {
-        timeout = setTimeout(inner, repeatWaitSeconds * 1000);
+        var took = finished - start;
+        var next = repeatWaitSeconds * 1000 - took;
+        if (next <= 0) next = 0;
+        timeout = setTimeout(inner, next);
       } else {
         process.kill(process.pid, signal);
       }
